2b3107746061453d68511a1d030075591518d7e3
Ganesha <reekoheek@gmail.com>
2014-04-23 16:53:11 +0700

    allow get on package non login

--- src/Xinix/Pax/Provider/AppProvider.php
+++ src/Xinix/Pax/Provider/AppProvider.php
@@ -8,11 +8,26 @@ class AppProvider extends \Bono\Provider\Provider
     {
         $app = $this->app;
 
+        $app->filter(
+            'auth.authorize',
+            function ($allowed) use ($app) {
+                if (is_null($allowed)) {
+                    if ($app->request->isGet()) {
+                        if ($app->request->getSegments(1) == 'package') {
+                            return true;
+                        }
+                    }
+                }
+                return $allowed;
+            }
+        );
+
         $app->get(
             '/',
             function () use ($app) {
                 $app->response->template('home');
             }
         );
+
     }
 }
